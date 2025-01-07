83023470103a388afa117162a1fb457d3879c182
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-07 18:29:12 +0530

    Add project files.

---
+++ 2_Pelendrome_or_Not/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pelendrome_Or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string abc = "shhd";
+            bool a = MyClass.Ispelendrome(abc, abc.Length);
+            if (a)
+            {
+                Console.WriteLine("Pelendrome");
+            }
+            else
+            {
+                Console.WriteLine("not pelendrome");
+            }
+            Console.ReadLine();
+        }
+    }
+    public static class MyClass
+    {
+        public static bool Ispelendrome(string value, int len)
+        {
+            for (int i = 0; i < len / 2; i++)
+            {
+                if (value[i] != value[len - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ DSA_Programe_String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revers_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String:");
+            string str = Console.ReadLine();
+            Console.WriteLine(myclass.Revers(str));
+            Console.ReadLine();
+        }
+    }
+    static class myclass
+    {
+        public static string Revers(string value)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                stringBuilder.Append(value[i]);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
