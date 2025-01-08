fd26db7180010e622a6f9d6186286a421e6dda8a
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-08 15:11:07 +0530

    Check if Strings Are Rotations of Each Other

---
+++ Check if Strings Are Rotations of Each Other/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_if_Strings_Are_Rotations_of_Each_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "aab";
+            string two = "aba";
+
+            bool flag = false;
+
+            char[] arr = one.ToCharArray();
+            char[] two2 = two.ToCharArray();
+
+            for (int i = 0; i < one.Length; i++)
+            {
+                if (two == new string(arr))
+                {
+                    flag = true;
+                    break;
+                }
+                char temp = arr[arr.Length - 1];
+                for (int j = arr.Length - 1; j > 0; j--)
+                {
+                    arr[j] = arr[j - 1];
+                }
+                arr[0] = temp;
+            }
+            if (flag)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Print all the duplicate characters in a string/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Print_all_the_duplicate_characters_in_a_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string ");
+            string a = Console.ReadLine();
+            Char[] name = a.ToCharArray();
+            Array.Sort(name);
+
+            int count = 1;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                for (int j = i; j < name.Length - 1; j++)
+                {
+                    if (name[j] == name[j + 1])
+                    {
+                        count++;
+                        i++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine(" " + name[i] + " " + count);
+                count = 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
