1d77bf62ef175413fe3b48039b941b7fcae541d3
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-10 18:46:41 +0530

    Look-and-Say Sequence

---
+++ Look-and-Say Sequence/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Look_and_Say_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "11";
+            Console.Write("Enter element");
+            int n = int.Parse(Console.ReadLine());
+            if (n == 1)
+            {
+                number = "1";
+                return;
+            }
+            if (n == 2)
+            {
+                number = "11";
+                return;
+            }
+            int count = 1;
+            string newstring = "";
+
+            //1, 11, 21, 1211, 111221, 312211, 13112221, 1113213211, 
+            for (int i = 3; i <= n; i++)
+            {
+                for (int j = 1; j < number.Length; j++)
+                {
+                    if (number[j] == number[j - 1])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        newstring += count.ToString() + number[j - 1];
+                        count = 1;
+                    }
+                }
+                newstring += count.ToString() + number[number.Length - 1];
+                number = "";
+                number = newstring;
+                newstring = "";
+                count = 1;
+            }
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Look-and-Say Sequence/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Look-and-Say Sequence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Look-and-Say Sequence")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("504d1214-9c88-40ff-9d11-7415907acd4b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
