a659b8b9ebb91680ce6ab44c8f240d00aada9c49
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-11 18:29:23 +0530

    Longest Pelendrome Substring

---
+++ Longest Palindromic Substring/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Longest_Palindromic_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //forgeeksskeegfor
+            string value = "forgeeksskeegfor";
+            string CurrentPelendrome;
+            string LongestPelendrome = string.Empty;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                for (int j = i + 1; j < value.Length; j++)
+                {
+                    if (MyClass.Ispelendrome(value, i, j))
+                    {
+                        CurrentPelendrome = value.Substring(i, j - i);
+                        if (CurrentPelendrome.Length > LongestPelendrome.Length)
+                        {
+                            LongestPelendrome = CurrentPelendrome;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Longest Pelendrome:" + LongestPelendrome);
+            Console.ReadLine();
+        }
+    }
+    public static class MyClass
+    {
+        public static bool Ispelendrome(string input, int start, int end)
+        {
+            end--;
+            while (start < end)
+            {
+                if (input[start] != input[end])
+                {
+                    return false;
+                }
+                end--;
+                start++;
+            }
+            return true;
+        }
+    }
+}
