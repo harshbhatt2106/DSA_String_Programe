cad904c7beb08a0185938c8ff286eab3b841f446
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-16 16:56:57 +0530

    Check if Strings Are Rotations of Each Other

---
+++ Check if Strings Are Rotations of Each Other/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_if_Strings_Are_Rotations_of_Each_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "aba";
+            string s2 = "aab";
+
+            char temp;
+            bool IsRotate = false;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1 == s2)
+                {
+                    IsRotate = true;
+                    break;
+                }
+                else
+                {
+                    temp = s1[i];
+                    s1 = temp + s1.Substring(0, s1.Length - 1);
+                }
+
+            }
+            if (IsRotate)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
