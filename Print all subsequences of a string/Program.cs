beab873389c24379b65914bacc30de7b316882b6
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-31 18:37:51 +0530

    Print all subsequences of a string

---
+++ Print all subsequences of a string/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Print_all_subsequences_of_a_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "Geeks";
+            string concatestring = "";
+            int len = value.Length;//5
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len - i; j++)
+                {
+                    concatestring = "";
+                    for (int k = j; k <= j + i; k++)
+                    {
+                        concatestring += value[k];
+                    }
+                    Console.WriteLine(concatestring);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
