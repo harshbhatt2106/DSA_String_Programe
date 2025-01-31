8456038ba8054b351b9c6cbc51ebf13e0dd379e9
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-31 18:36:58 +0530

    Rabin-Krap

---
+++ Rabin-Karp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabin_Karp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input = "HARSH BHATT HARSH";
+            string Find = "HAR";
+
+            bool IsFind = false;
+            int MatchIndex = 0;
+            int index = 0;
+
+            for (int i = 0; i < Input.Length; i++)
+            {
+                IsFind = false;
+                index = i;
+                for (int j = 0; j < Find.Length; j++)
+                {
+                    if (Input[index] == Find[j])
+                    {
+                        IsFind = true;
+                        MatchIndex = i;
+                        if (i < Input.Length - 1)
+                        {
+                            index++;
+                        }
+                    }
+                    else
+                    {
+                        IsFind = false;
+                        MatchIndex = 0;
+                        break;
+                    }
+                }
+                if (IsFind)
+                {
+                    Console.WriteLine(MatchIndex);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
