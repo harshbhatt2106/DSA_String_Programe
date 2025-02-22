edcd0d7cb814712691fac7e95cd883141f80c2f6
harshbhatt2106 <harshbhatt932@gmail.com>
2025-02-22 10:00:25 +0530

    Longest_Common_SubSequence

---
+++ Longest_Common_SubSequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Common_SubSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = "HARSHBHATT";
+            string input2 = "HABSBHATT";
+            string answer = "";
+
+            int index;
+            for (int i = 0; i < input1.Length; i++)
+            {
+                index = i;
+                string current = "";
+                for (int j = 0; j < input2.Length; j++)
+                {
+                    if (index < input1.Length && input1[index] == input2[j])
+                    {
+                        current += input2[j];
+                        index++;
+                    }
+                }
+                if (current.Length > answer.Length)
+                {
+                    answer = current;
+                }
+            }
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+        }
+    }
+}
