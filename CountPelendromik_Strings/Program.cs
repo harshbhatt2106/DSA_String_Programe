4b145a9c52ae19d080def8551f71afe7e2d78fcf
harshbhatt2106 <harshbhatt932@gmail.com>
2025-02-22 09:59:40 +0530

    CountPelendromik_Strings

---
+++ CountPelendromik_Strings/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountPelendromik_Strings
+{
+    internal class Program
+    {
+        public static bool Ispelendrome(string input)
+        {
+            bool Ispelendrome = true;
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - i - 1])
+                {
+                    Ispelendrome = false;
+                }
+            }
+            return Ispelendrome;
+        }
+
+        static void Main(string[] args)
+        {
+            string Input = "harsh";
+            int count = 0;
+            string concatestring = "";
+            int len = Input.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = i + 1; j < len; j++)
+                {
+                    concatestring = "";
+                    concatestring += Input[i];
+                    concatestring += Input[j];
+                    concatestring += Input[i];
+                    if (Program.Ispelendrome(concatestring))
+                    {
+                        count++;
+                    }
+
+                    Console.WriteLine(concatestring);
+                }
+            }
+            Console.WriteLine(count);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CountPelendromik_Strings/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CountPelendromik_Strings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CountPelendromik_Strings")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca2d4e0a-f2ef-42cf-bd3b-5fa198e5c3b3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
